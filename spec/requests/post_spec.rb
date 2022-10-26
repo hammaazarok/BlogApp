ee93df563d5bddad247af873cb057f9c23c679e1
Hamma Azarok <hammaagazarok@gmail.com>
2022-10-26 10:21:05 +0200

    add controller specs

---
+++ spec/requests/post_spec.rb
@@ -0,0 +1,42 @@
+require "rails_helper"
+
+RSpec.describe Post, :type => :request do
+context "Index Action" do
+    before(:each) do
+        get "/users/#{User.first.id}/posts/"
+    end
+it "index action renders the correct template" do
+        expect(response).to render_template(:index)
+      end
+  it "index respone is correct" do
+    expect(response.status).to eq(200)
+  end
+  
+
+  it "the response body includes correct placeholder text" do
+    expect(response.body).to include("Complete list of all posts made by the user")
+  end
+
+end
+
+context "Show Action" do
+    before(:each) do
+        get "/users/#{User.first.id}/posts/#{Post.first.id}"
+    end
+
+    it "show action renders the correct template" do
+            expect(response).to render_template(:show)
+          end
+      it "show respone is correct" do
+        expect(response.status).to eq(200)
+      end
+      
+    
+      it "the response body includes correct placeholder text" do
+        expect(response.body).to include("Details of the post made by the user")
+      end
+    
+    end
+
+  
+end
