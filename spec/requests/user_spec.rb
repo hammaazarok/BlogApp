6c3a9f905865ebf79dc199ce435ffe34d18c6351
Hamma Azarok <hammaagazarok@gmail.com>
2022-10-26 10:24:23 +0200

    fix linters

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -1,42 +1,36 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe User, :type => :request do
-context "Index Action" do
+RSpec.describe User, type: :request do
+  context 'Index Action' do
     before(:each) do
-        get "/users/"
+      get '/users/'
     end
-it "index action renders the correct template" do
-        expect(response).to render_template(:index)
-      end
-  it "index respone is correct" do
-    expect(response.status).to eq(200)
+    it 'index action renders the correct template' do
+      expect(response).to render_template(:index)
+    end
+    it 'index respone is correct' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'the response body includes correct placeholder text' do
+      expect(response.body).to include('Complete list of all users in the app')
+    end
   end
-  
 
-  it "the response body includes correct placeholder text" do
-    expect(response.body).to include("Complete list of all users in the app")
-  end
-
-end
-
-context "Show Action" do
+  context 'Show Action' do
     before(:each) do
-        get "/users/1"
+      get '/users/1'
     end
 
-    it "show action renders the correct template" do
-            expect(response).to render_template(:show)
-          end
-      it "show respone is correct" do
-        expect(response.status).to eq(200)
-      end
-      
-    
-      it "the response body includes correct placeholder text" do
-        expect(response.body).to include("Display user's details based on the User ID")
-      end
-    
+    it 'show action renders the correct template' do
+      expect(response).to render_template(:show)
+    end
+    it 'show respone is correct' do
+      expect(response.status).to eq(200)
     end
 
-  
+    it 'the response body includes correct placeholder text' do
+      expect(response.body).to include("Display user's details based on the User ID")
+    end
+  end
 end
