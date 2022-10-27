6873bae2ca0f68c6e2693f1c333d97ec629cca2e
Hamma Azarok <hammaagazarok@gmail.com>
2022-10-27 16:28:01 +0200

    add style

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :post, foreign_key: 'author_id'
   has_many :comment, foreign_key: 'author_id'
   has_many :like, foreign_key: 'author_id'
-
+  
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
